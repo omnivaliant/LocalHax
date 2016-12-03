eca3edbafd1ad42afbb4a523b11d3c1e17997665
Jackie Xu <jackiexu2007@hotmail.com>
2016-12-03 15:47:29 -0500

    Added Quack! message and the toggle switch

--- LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
+++ LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ToggleButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnGoose;
     TextView txtMain;
+    ToggleButton tgl00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         btnGoose = (Button)findViewById(R.id.goose);
         txtMain = (TextView)findViewById(R.id.main);
+        tgl00 = (ToggleButton)findViewById(R.id.b00);
 
         btnGoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtMain.setText("@string/goose_click");
+                txtMain.setText("Quack!");
+                // btnGoose.setSelected(true);
+                tgl00.toggle();
             }
         });
 
