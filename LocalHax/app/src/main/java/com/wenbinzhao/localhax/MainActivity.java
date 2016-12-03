6dac2c847ef509588f807ede94c59768c8194e74
Jackie Xu <jackiexu2007@hotmail.com>
2016-12-03 15:29:29 -0500

    Made a button working

--- LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
+++ LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
@@ -1,13 +1,34 @@
 package com.wenbinzhao.localhax;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGoose;
+    TextView txtMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGoose = (Button)findViewById(R.id.goose);
+        txtMain = (TextView)findViewById(R.id.main);
+
+        btnGoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtMain.setText("@string/goose_click");
+            }
+        });
+
     }
+
+
+
 }
