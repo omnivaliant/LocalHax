d9dd63b74d91a0dd86151904e69372628c0488d0
Ben Zhao <zhaoben99@gmail.com>
2016-12-03 15:52:21 -0500

    Layout done

--- LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
+++ LocalHax/app/src/main/java/com/wenbinzhao/localhax/MainActivity.java
@@ -2,6 +2,7 @@ package com.wenbinzhao.localhax;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
